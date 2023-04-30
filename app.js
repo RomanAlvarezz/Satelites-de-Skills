9f8b5b98d608551b752d07a391b930d5f6960255
Roman <alvarezroman150@gmail.com>
2023-04-30 15:30:11 -0300

    funcionamiento básico con js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-// const d = document;
+const d = document;
 
-// d.addEventListener('click', e => {
-//     // console.log(e.target)
-//     if (e.target.matches('.ring-3')){
-//         console.log('eiwa')
-//         let $moon = d.querySelector('.moon-1-txt');
-//         console.log($moon);
-//         $moon.classList.toggle('moon-1-txt-active')
-//     }
-// })
+d.addEventListener('click', e => {
+    //console.log(e.target);
+    const $element = e.target;
+    if($element.matches('.moon__1') ||
+    $element.matches('.moon__content-1')){
+        d.querySelector('.moon__content-1').classList.toggle('moon__content-1-active');
+        //console.log('has clickeado el icono con html')
+    }
+
+
+
+
+
+    // if (e.target.matches('.ring-3')){
+    //     console.log('eiwa')
+    //     let $moon = d.querySelector('.moon-1-txt');
+    //     console.log($moon);
+    //     $moon.classList.toggle('moon-1-txt-active')
+    // }
+})
