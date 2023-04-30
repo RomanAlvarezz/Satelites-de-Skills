7c637aba6b405755a80659c88dc941538cdd6a76
Roman <alvarezroman150@gmail.com>
2023-04-30 00:04:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// const d = document;
+
+// d.addEventListener('click', e => {
+//     // console.log(e.target)
+//     if (e.target.matches('.ring-3')){
+//         console.log('eiwa')
+//         let $moon = d.querySelector('.moon-1-txt');
+//         console.log($moon);
+//         $moon.classList.toggle('moon-1-txt-active')
+//     }
+// })
